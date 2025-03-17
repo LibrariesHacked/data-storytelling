180913f4066050146ddc964423003fd89c77e78a
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-03-17 12:53:38 +0000

    Change pdf library

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,12 +1,13 @@
 function printPDF () {
-  // The file name is the end of the URL
   var fileName = window.location.pathname.split('/').pop()
   const element = document.getElementsByClassName('post')[0]
   const opt = {
     margin: 1,
-    filename: `${fileName}.pdf`
+    filename: `${fileName}.pdf`,
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
   }
 
-  // Old monolithic-style usage:
   html2pdf(element, opt)
 }
