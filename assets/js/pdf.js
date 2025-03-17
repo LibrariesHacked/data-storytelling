726a55216dad518491bf93f56734b2ad23dc5a58
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-03-17 12:25:02 +0000

    Adjusting print options

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -6,8 +6,7 @@ function printPDF () {
     margin: 1,
     filename: `${fileName}.pdf`,
     image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
   }
 
   // Old monolithic-style usage:
