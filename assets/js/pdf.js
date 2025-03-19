f168d03b7dc6f416d67abe439d1012f1c442b716
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-03-19 08:33:14 +0000

    Overrinding default blue

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -2,22 +2,13 @@ function printPDF () {
   var fileName = window.location.pathname.split('/').pop()
   const element = document.getElementsByClassName('post')[0]
   const opt = {
-    margin: 40,
+    // Use A4 margins
+    margin: 10,
     filename: `${fileName}.pdf`,
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 2 },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
     pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
   }
-
-  // No modes, only explicit elements.
-  html2pdf().set({
-    pagebreak: {
-      before: '.beforeClass',
-      after: ['#after1', '#after2'],
-      avoid: 'img'
-    }
-  })
-
   html3pdf(element, opt)
 }
