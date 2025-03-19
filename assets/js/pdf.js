4cff13756b9d51e7b4a4118b06370a74d5cb2f03
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-03-19 08:19:28 +0000

    Adjusting pdf options

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -2,12 +2,22 @@ function printPDF () {
   var fileName = window.location.pathname.split('/').pop()
   const element = document.getElementsByClassName('post')[0]
   const opt = {
-    margin: 1,
+    margin: 40,
     filename: `${fileName}.pdf`,
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 2 },
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
   }
+
+  // No modes, only explicit elements.
+  html2pdf().set({
+    pagebreak: {
+      before: '.beforeClass',
+      after: ['#after1', '#after2'],
+      avoid: 'img'
+    }
+  })
 
   html3pdf(element, opt)
 }
